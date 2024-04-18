bc8ff41952bace66916fdf7aca137080fd20d430
mianhusnain1 <mianhusnain550@gmail.com>
2024-04-18 21:42:23 +0500

    Update

---
+++ lib/Screens/navbar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+class Navbar extends StatelessWidget {
+  const Navbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+        UserAccountsDrawerHeader(
+          accountName: Text(""), 
+          accountEmail: Text(""),
+          currentAccountPicture: CircleAvatar(
+            child: ClipOval(
+              child: Image.asset("Assets/images/Profile.jpg"),
+            ) ,
+          ),
+        
+          )
+        ],
+        
+      ),
+    );
+  }
+}
