e4a6a8f699e91a4a1a2d95b8ac61940e6e1c5748
usman1170 <usman.zrr@gmail.com>
2024-04-23 19:30:23 +0500

    com

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:doctor/Screens/splash.dart';
 import 'package:doctor/firebase_options.dart';
-import 'package:doctor/routes/routes.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
