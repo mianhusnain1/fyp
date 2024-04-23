a03fb8ccd1490c3cb0500c017ddd02601d72dde5
usman1170 <usman.zrr@gmail.com>
2024-04-23 19:29:15 +0500

    com

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:doctor/Screens/splash.dart';
 import 'package:doctor/firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -6,33 +5,27 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform );
-  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
-  
 }
+
 const Color myColor = Color(0xFF72DDE8);
 const Color myColor1 = Color(0xFF1D5761);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true, 
-        
-      ),
-      home: const Splash()
-      //  const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Splash()
+        //  const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
   }
 }
-
