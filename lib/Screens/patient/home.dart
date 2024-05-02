16695c7a80acb64546b54067b559f111f53446da
mianhusnain1 <mianhusnain550@gmail.com>
2024-05-02 14:17:26 +0500

    Authentication

--- lib/Screens/patient/home.dart
+++ lib/Screens/patient/home.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:doctor/ai%20assistance/screens/chat_screen.dart';
+import 'package:doctor/widgets/health_needs.dart';
 import 'package:doctor/widgets/navbar.dart';
 import 'package:doctor/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class PatientHome extends StatefulWidget {
   const PatientHome({super.key});
@@ -20,8 +24,40 @@ class _PatientHomeState extends State<PatientHome> {
         backgroundColor: darkColor,
       ),
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 22
+          .0),
+          child: const Column(
+            // qst Column
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                child: Text(
+                  "Hi, Name",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: darkColor,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                child: Text(
+                  "How are you feeling?",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: darkColor),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Column(
           children: [
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.06,
@@ -51,6 +87,7 @@ class _PatientHomeState extends State<PatientHome> {
                 width: MediaQuery.of(context).size.width - 40,
                 padding: const EdgeInsets.only(left: 14),
                 decoration: BoxDecoration(
+                  color: lightColor.withOpacity(0.2),
                   borderRadius: BorderRadius.circular(20),
                   border: Border.all(color: darkColor),
                 ),
@@ -77,14 +114,23 @@ class _PatientHomeState extends State<PatientHome> {
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
             ),
+            const SizedBox(child: HealthNeeds())
           ],
-        ),
-      ),
+        )
+      ]),
+      // bottomNavigationBar:
+      //     BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
+      //   BottomNavigationBarItem(
+      //     icon: Icon(Icons.home_outlined),
+      //     activeIcon: Icon(Icons.home),
+      //     label: "Home",
+      //   )
+      // ]),
     );
   }
 }
---
+++ lib/widgets/health_needs.dart
@@ -0,0 +1,170 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:doctor/main.dart';
+import 'package:flutter/material.dart';
+
+class HealthNeeds extends StatelessWidget {
+  const HealthNeeds({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<CustomIcon> customIcons = [
+      CustomIcon(name: "Appointment", icon: 'assets/imgs/appointment.png'),
+      CustomIcon(name: "Hospital", icon: 'assets/imgs/hospital.png'),
+      CustomIcon(name: "Covid-19", icon: 'assets/imgs/virus.png'),
+      CustomIcon(name: "More", icon: 'assets/imgs/more.png'),
+    ];
+    List<CustomIcon> healthNeeds = [
+      CustomIcon(name: "Appointment", icon: 'assets/imgs/appointment.png'),
+      CustomIcon(name: "Hospital", icon: 'assets/imgs/hospital.png'),
+      CustomIcon(name: "Covid-19", icon: 'assets/imgs/virus.png'),
+      CustomIcon(name: "Pharmacy", icon: 'assets/imgs/drug.png'),
+    ];
+    List<CustomIcon> specialisedCared = [
+      CustomIcon(name: "Diabetes", icon: 'assets/imgs/blood.png'),
+      CustomIcon(name: "Health Care", icon: 'assets/imgs/health_care.png'),
+      CustomIcon(name: "Dental", icon: 'assets/imgs/tooth.png'),
+      CustomIcon(name: "Insured", icon: 'assets/imgs/insurance.png'),
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: List.generate(customIcons.length, (index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  if (index == customIcons.length - 1) {
+                    showModalBottomSheet(
+                      context: context,
+                      showDragHandle: false,
+                      builder: (context) {
+                        return Container(
+                          color: Colors.white,
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(20),
+                          height: 350,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // HEALTH NEEDS SECTION
+                              const Text("Health Needs",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                              const SizedBox(height: 15),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: List.generate(
+                                  healthNeeds.length,
+                                  (index) {
+                                    return Column(
+                                      children: [
+                                        InkWell(
+                                          onTap: () {},
+                                          borderRadius:
+                                              BorderRadius.circular(90),
+                                          child: Container(
+                                            width: 60,
+                                            height: 60,
+                                            padding: const EdgeInsets.all(15),
+                                            decoration: BoxDecoration(
+                                              color:
+                                                  lightColor.withOpacity(0.2),
+                                              shape: BoxShape.circle,
+                                            ),
+                                            child: Image.asset(
+                                              healthNeeds[index].icon,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(height: 6),
+                                        Text(healthNeeds[index].name)
+                                      ],
+                                    );
+                                  },
+                                ),
+                              ),
+                              const SizedBox(height: 30),
+                              // SPECIALISED CARE SECTION
+
+                              const Text("Specialised Care",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                              const SizedBox(height: 15),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: List.generate(
+                                  specialisedCared.length,
+                                  (index) {
+                                    return Column(
+                                      children: [
+                                        InkWell(
+                                          onTap: () {},
+                                          borderRadius:
+                                              BorderRadius.circular(90),
+                                          child: Container(
+                                            width: 60,
+                                            height: 60,
+                                            padding: const EdgeInsets.all(15),
+                                            decoration: BoxDecoration(
+                                              color:
+                                                  lightColor.withOpacity(0.2),
+                                              shape: BoxShape.circle,
+                                            ),
+                                            child: Image.asset(
+                                              specialisedCared[index].icon,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(height: 6),
+                                        Text(specialisedCared[index].name)
+                                      ],
+                                    );
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+                borderRadius: BorderRadius.circular(90),
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: lightColor.withOpacity(0.2),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.asset(
+                    customIcons[index].icon,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 6),
+              Text(customIcons[index].name)
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class CustomIcon {
+  final String name;
+  final String icon;
+
+  CustomIcon({
+    required this.name,
+    required this.icon,
+  });
+}
